dad2c73879d6b3ed2d09c10023e13d4abbc3ad37
rogeriopaiva13 <rogeriopaiva13@gmail.com>
2026-05-16 11:38:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div style={{ padding: 30 }}>
       <h1>Meu Salário Organizado</h1>
-      <p>App funcionando ✅</p>
+      <p>Aplicativo funcionando ✅</p>
     </div>
   )
 }

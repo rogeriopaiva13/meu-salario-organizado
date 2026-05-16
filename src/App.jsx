7752606f0cedfd46c9c9e64f204e4fcfab7202af
rogeriopaiva13 <rogeriopaiva13@gmail.com>
2026-05-16 10:10:22 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,8 @@
-import { useState } from 'react'
-
 export default function App() {
-  const [salario,setSalario]=useState('')
-  const [contas,setContas]=useState('')
-
-  const saldo =
-    Number(salario)-Number(contas)
-
-  return(
-    <div style={{
-      maxWidth:'400px',
-      margin:'20px auto',
-      padding:'20px'
-    }}>
-      <h1>💰 Meu Salário Organizado</h1>
-
-      <input
-      placeholder="Salário"
-      value={salario}
-      onChange={(e)=>setSalario(e.target.value)}
-      />
-
-      <br/><br/>
-
-      <input
-      placeholder="Contas"
-      value={contas}
-      onChange={(e)=>setContas(e.target.value)}
-      />
-
-      <h2>
-      Saldo: R$ {saldo}
-      </h2>
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Meu Salário Organizado</h1>
+      <p>App funcionando ✅</p>
     </div>
   )
-}
+}}

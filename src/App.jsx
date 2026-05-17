ae58522715c82aec8f67113a316726f95540087a
rogeriopaiva13 <rogeriopaiva13@gmail.com>
2026-05-16 21:38:04 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import React, { useState, useEffect } from 'react';
 import { useState } from 'react'
-
 export default function App() {
   const [salario,setSalario]=useState('')
   const [contas,setContas]=useState('')

0e4c70a11a93f9d60e7709890b0d675a5cf1eeb1
rogeriopaiva13 <rogeriopaiva13@gmail.com>
2026-05-19 00:38:19 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVPsaa7ZZpwJpprLLwAGJhM8Bb8pId_SQ",
@@ -13,3 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
+export const auth = getAuth(app);
+
+export const provider = new GoogleAuthProvider();

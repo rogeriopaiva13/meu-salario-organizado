c69e46e6381679e4fe080e5be901165abce342f2
rogeriopaiva13 <rogeriopaiva13@gmail.com>
2026-05-18 23:44:04 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAVPsaa7ZZpwJpprLLwAGJhM8Bb8pId_SQ",
+  authDomain: "meu-salario-organizado.firebaseapp.com",
+  projectId: "meu-salario-organizado",
+  storageBucket: "meu-salario-organizado.firebasestorage.app",
+  messagingSenderId: "421726461241",
+  appId: "1:421726461241:web:e15494e468671538cb9ce8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;

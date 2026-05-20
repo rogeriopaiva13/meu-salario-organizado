fcf1ea3608f1b8fd5ece6b830b4b46c32aa9bbeb
rogeriopaiva13 <rogeriopaiva13@gmail.com>
2026-05-20 12:09:44 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(
-  document.getElementById('root')
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => {
+        console.log("Service Worker registrado!");
+      })
+      .catch((error) => {
+        console.log("Erro Service Worker:", error);
+      });
+  });
+}
